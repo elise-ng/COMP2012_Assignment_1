75ca6ba6812b1b271402da810e8d67037361c67a
Elliot Ng <chihimng.io@gmail.com>
2018-03-01 23:11:29 +0800

    implement Product methods

--- COMP2012_Assignment_1/COMP2012_Assignment_1/product.cpp
+++ COMP2012_Assignment_1/COMP2012_Assignment_1/product.cpp
@@ -1,3 +1,60 @@
 //Submit this file
 #include "product.h"
 //You are NOT allowed to include any additional library
+
+Product::Product(string name, ProductType type, float price) {
+    this->name = name;
+    this->type = type;
+    this->price = price;
+    this->quantity = 0;
+}
+
+string Product::getName() {
+    return this->name;
+}
+
+ProductType Product::getType() {
+    return this->type;
+}
+
+float Product::getPrice() {
+    return this->price;
+}
+
+int Product::getQuantity() {
+    return this->quantity;
+}
+
+bool Product::setPrice(float price) {
+    if (price > 0) {
+        this->price = price;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool Product::setQuantity(int quantity) {
+    if (quantity >= 0) {
+        this->quantity = quantity;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+int Product::compare(Product *other) {
+    if (this->getPrice() > other->getPrice()) {
+        return 1;
+    } else if (this->getPrice() < other->getPrice()) {
+        return -1;
+    } else {
+        if (this->getName() > other->getName()) {
+            return 1;
+        } else if (this->getName() < other->getName()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+}
