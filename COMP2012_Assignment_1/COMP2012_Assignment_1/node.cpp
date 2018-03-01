8ffc9888834ab14a704c9e72202cfc9c5b0e7efa
Elliot Ng <chihimng.io@gmail.com>
2018-03-01 22:50:57 +0800

    implement Node methods

--- COMP2012_Assignment_1/COMP2012_Assignment_1/node.cpp
+++ COMP2012_Assignment_1/COMP2012_Assignment_1/node.cpp
@@ -1,3 +1,29 @@
 //Submit this file
 #include "node.h"
 //You are NOT allowed to include any additional library
+
+Node::Node(Shop* shop, Node* next) {
+    this->shop = shop;
+    this->next = next;
+}
+
+Node::~Node() {
+    delete this->shop;
+    this->shop = nullptr;
+}
+
+Shop* Node::getShop() {
+    return this->shop;
+}
+
+Node* Node::getNext() {
+    return this->next;
+}
+
+void Node::setShop(Shop *shop) {
+    this->shop = shop;
+}
+
+void Node::setNext(Node *node) {
+    this->next = node;
+}
